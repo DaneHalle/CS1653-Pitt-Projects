663233781c7c725bcd08fb354d07673e13b96221
jkomalley <jko12@pitt.edu>
2020-09-28 22:33:51 -0400

    Updated uploadf gui

--- src/ClientGui.java
+++ src/ClientGui.java
@@ -55,26 +55,6 @@ public class ClientGui{
 		String[] get_prompts = {"Enter username"};
 		get_button.addActionListener(new arbAction(rcli, "get", get_prompts));
 
-		// show a group within a user's scope
-		JButton show_button = new JButton("Escalate Permissions");
-		String[] show_prompts = {"Enter groupname"};
-		show_button.addActionListener(new arbAction(rcli, "show", show_prompts));
-
-		// show all groups within a user's scope
-		JButton showall_button = new JButton("Escalate All");
-		String[] showall_prompts = {};
-		showall_button.addActionListener(new arbAction(rcli, "showall", showall_prompts));
-
-		// hide a group from a user's scope
-		JButton hide_button = new JButton("De-escalate Permissions");
-		String[] hide_prompts = {"Enter groupname"};
-		hide_button.addActionListener(new arbAction(rcli, "hide", hide_prompts));
-
-		// hide all groups from a user's scope
-		JButton hideall_button = new JButton("De-escalate All");
-		String[] hideall_prompts = {};
-		hideall_button.addActionListener(new arbAction(rcli, "hideall", hideall_prompts));
-
 		// create user
 		JButton cuser_button = new JButton("Create User");
 		String[] cuser_prompts = {"Enter new username"};
@@ -138,19 +118,9 @@ public class ClientGui{
 		String[] status_prompts = {};
 		status_button.addActionListener(new arbAction(rcli, "status", status_prompts));
 
-		// help
-		JButton help_button = new JButton("HELP");
-		String[] help_prompts = {};
-		help_button.addActionListener(new arbAction(rcli, "help", help_prompts));
-
-
-		JPanel action_panel = new JPanel(new GridLayout(18,1));
+		JPanel action_panel = new JPanel(new GridLayout(13,1));
 		// action_panel.setLayout(new BoxLayout(action_panel, BoxLayout.Y_AXIS));
 		action_panel.add(get_button);
-		action_panel.add(show_button);
-		action_panel.add(showall_button);
-		action_panel.add(hide_button);
-		action_panel.add(hideall_button);
 		action_panel.add(cuser_button);
 		action_panel.add(duser_button);
 		action_panel.add(cgroup_button);
@@ -163,7 +133,6 @@ public class ClientGui{
 		action_panel.add(downloadf_button);
 		action_panel.add(deletef_button);
 		action_panel.add(status_button);
-		action_panel.add(help_button);
 
 		//layout
 		frame.add(menu_bar, BorderLayout.NORTH);
@@ -203,11 +172,10 @@ public class ClientGui{
 				if(temp != null) actionOptions = actionOptions + " " + temp;
 				else flag = false;
 			}
-			System.out.println("Action: " + action.toUpperCase() + " " + actionOptions);
+			System.out.println("Action: " + action + " " + actionOptions);
 			StringTokenizer cmd = new StringTokenizer(action + " " + actionOptions);
 
 			if(flag) rcli.mapCommand(cmd);
-			else System.out.println("\tAborted action");
 		}
 	}
 
@@ -229,6 +197,13 @@ public class ClientGui{
 				file = fc.getSelectedFile();
 				System.out.println("Chose file (" + file.getName() + ") to upload");
 				System.out.println("Path: " + file.getPath());
+
+				String dest = JOptionPane.showInputDialog("Enter Destination Name");
+				String gn = JOptionPane.showInputDialog("Enter Group Name");
+				if(dest != null && gn != null){
+					StringTokenizer cmd = new StringTokenizer("uploadf " + file.getPath() + " " + dest + " " + gn);
+					rcli.mapCommand(cmd);
+				}
 			}else{
 				System.out.println("Error choosing file");
 			}
